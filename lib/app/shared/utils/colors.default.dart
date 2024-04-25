5e33084f18bdc05702274d2768185bc48894027d
Gabriel Nascimento <gabriel.nascimento@dcomp.ufs.br>
2024-04-25 08:23:02 -0300

    arquivo das cores do projeto

---
+++ lib/app/shared/utils/colors.default.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorsDefault {
+  static Color get primaryBlue => const Color(0xFF007FBA);
+  static Color get secondaryBlue => const Color(0xFF99CCE3);
+  static Color get switchSecondaryBlue => const Color(0xFF007FBA);
+  static Color get borderColorPrimary => const Color(0xFFCFD8DC);
+  static Color get primaryTextBlack => const Color(0xFF403E43);
+  static Color get secondaryTextBlack => const Color(0xFF565656);
+  static Color get primaryGrey => const Color(0xFF455A64);
+  static Color get textFormBorderGrey => const Color(0xFFDEDEDE);
+}
